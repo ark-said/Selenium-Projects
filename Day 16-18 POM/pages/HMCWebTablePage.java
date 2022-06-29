715f08c1d766c24108b366f5c8b4573b90d052f5
Ali Said Ark <arkalisaid@gmail.com>
2022-06-29 21:20:52 +0300

    v5

---
+++ Day 16-18 POM/pages/HMCWebTablePage.java
@@ -0,0 +1,57 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class HMCWebTablePage {
+    public HMCWebTablePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy (xpath = "//thead//tr[1]//th")
+    public List<WebElement> headerBirinciSatirDatalar;
+
+    @FindBy (xpath = "//tbody")
+    public WebElement tumBodyWebElementi;
+
+    @FindBy (xpath = "//tbody//td")
+    public List<WebElement> tumBodyDatalariList;
+
+    @FindBy(xpath = "//tbody//tr")
+    public List<WebElement> satirlarListesi;
+
+    public WebElement satirYazdir(int satirNo){
+
+        String satirDinamikXpath="//tbody//tr["+ satirNo +"]";
+        WebElement satirElementi=Driver.getDriver().findElement(By.xpath(satirDinamikXpath));
+
+        return satirElementi;
+    }
+
+    public String hucreWebElementGetir(int satir, int sutun) {
+
+        String dinamikHucreXpath="//tbody//tr["+ satir +"]//td["+ sutun+"]";
+        WebElement istenenHucreElementi=Driver.getDriver().findElement(By.xpath(dinamikHucreXpath));
+        String hucreDatasi=istenenHucreElementi.getText();
+
+        return hucreDatasi;
+    }
+
+    public void sutunYazdir(int sutun) {
+
+        // her bir satirdaki istenen sutun elementini yazdirabilmek icin
+        // once satir sayisini bilmeye ihtiyacim var
+
+        int satirsayisi=satirlarListesi.size();
+
+        for (int i=1 ; i<=satirsayisi ; i++){
+            System.out.println(hucreWebElementGetir(i, sutun));
+        }
+
+    }
+}
---
+++ Day 16-18 POM/tests/day18/C03_Webtables.java
@@ -0,0 +1,62 @@
+package tests.day18;
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import pages.HMCWebTablePage;
+import pages.HotelMyCampPage;
+
+import java.util.List;
+
+public class C03_Webtables {
+
+    HotelMyCampPage hotelMyCampPage;
+    HMCWebTablePage hmcWebTablePage;
+
+    @Test
+    public void loginT(){
+
+        hotelMyCampPage=new HotelMyCampPage();
+        hotelMyCampPage.hotelMyCampLogin();
+    }
+
+    @Test (dependsOnMethods = "loginT")
+    public void table(){
+
+        hmcWebTablePage=new HMCWebTablePage();
+        List<WebElement> headerDataList= hmcWebTablePage.headerBirinciSatirDatalar;
+        System.out.println("tablodaki sutun sayisi : " + headerDataList);
+
+        System.out.println(hmcWebTablePage.tumBodyWebElementi.getText());
+        // eger bodyyi tek bir webelement olarak locate edersek
+        // icindeki tum datalari getText() ile yazdirabiliriz
+        // ancak bu durumda bu elementler ayri ayri degil
+        // bodynin icindeki tek bir stringin parcalari olurlar
+        // dolayisiyla bu elementlere tek tek ulasmamin mumkun olmaz
+        // sadece contains methodu ile bodyde olup olmadiklarini test edebiliriz
+
+        // eger her bir datayi ayri ayri almak istersek
+        // //tbody//td seklinde locate edip bir list'e atabiliriz
+
+        List<WebElement> bodyTumDataList=hmcWebTablePage.tumBodyDatalariList;
+
+        System.out.println("body'deki data sayisi : " + bodyTumDataList.size());
+    }
+
+    @Test (dependsOnMethods = "loginT")
+    public void printRows(){
+        //1-
+        hmcWebTablePage=new HMCWebTablePage();
+        System.out.println(hmcWebTablePage.satirlarListesi.size());
+
+        //2-
+        List<WebElement> satirlarWebelementListesi=hmcWebTablePage.satirlarListesi;
+        for (WebElement each:satirlarWebelementListesi
+        ) {
+            System.out.println(each.getText());
+        }
+        //3-
+        System.out.println("4. satir : " + satirlarWebelementListesi.get(3).getText());
+
+    }
+
+}
